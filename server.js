5e9fe084c2a628b248f12cfc5ab1a9a22aabfed0
ariffurqon <arif.furqon@gmail.com>
2017-05-09 10:17:50 -0700

    modified port in the server

--- server.js
+++ server.js
@@ -3,8 +3,9 @@
 // require express framework and additional modules
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
-var app = express();
 var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 8080;
 
 // use ejs and express layouts
 app.set('view engine', 'ejs');
@@ -14,11 +15,6 @@ app.use(expressLayouts);
 // tell app to use bodyParser middleware
 app.use(bodyParser.urlencoded({extended: true}));
 
-// set up root route to respond with strings
-// app.get('/', function (req, res) {
-//   res.send('Oh hello lover..');
-// });
-
 // route our app
 var router = require('./app/routes');
 app.use('/', router);
@@ -26,7 +22,7 @@ app.use('/', router);
 // set static files (css and images, etc) location
 app.use(express.static(__dirname + '/public'));
 
-// listen on port 3000
-app.listen(3000, function () {
-  console.log('server started on localhost:3000');
+// start the server
+app.listen(port, function () {
+  console.log('app started');
 });
